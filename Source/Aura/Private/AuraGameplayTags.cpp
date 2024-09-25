b99fa4147d387d5dfc00976842c383edcb0171a7
Tom Casiello <tomcasiello@gmail.com>
2024-09-24 20:59:34 -0700

    Debuff Tags

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -188,6 +188,30 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 		);
 
 	/**
+	 ** Debuffs
+	 **/
+
+	GameplayTags.Debuff_Damned = UGameplayTagsManager::Get().AddNativeGameplayTag(
+	FName("Debuff.Damned"),
+	FString("Debuff for Arcane Damage, as the Target loses their soul to the depths of Hell")
+	);
+
+	GameplayTags.Debuff_Burn = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Burn"),
+		FString("Debuff for Fire Damage, as the flames continue to incinerate the Target")
+		);
+
+	GameplayTags.Debuff_Wounded = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Wounded"),
+		FString("Debuff for Physical Damage, as the Target sustains brutal injuries in battle")
+		);
+
+	GameplayTags.Debuff_Stun = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Debuff.Stun"),
+		FString("Debuff for Lightning Damage, ass the Target receives a paralyzing shock to their nervous system")
+		);
+
+	/**
 	 ** Meta Attributes
 	 **/
 
@@ -204,7 +228,16 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.DamageTypesToResistances.Add(GameplayTags.Damage_Lightning, GameplayTags.Attributes_Resistances_Lightning);
 	GameplayTags.DamageTypesToResistances.Add(GameplayTags.Damage_Arcane, GameplayTags.Attributes_Resistances_Arcane);
 	GameplayTags.DamageTypesToResistances.Add(GameplayTags.Damage_Physical, GameplayTags.Attributes_Resistances_Physical);
-	
+
+	/*
+	** Map of Damage Types to Debuffs
+	*/
+
+	GameplayTags.DamageTypesToDebuffs.Add(GameplayTags.Damage_Fire, GameplayTags.Debuff_Burn);
+	GameplayTags.DamageTypesToDebuffs.Add(GameplayTags.Damage_Lightning, GameplayTags.Debuff_Stun);
+	GameplayTags.DamageTypesToDebuffs.Add(GameplayTags.Damage_Arcane, GameplayTags.Debuff_Damned);
+	GameplayTags.DamageTypesToDebuffs.Add(GameplayTags.Damage_Physical, GameplayTags.Debuff_Wounded);
+
 	 /*
 	 ** Effects
 	 */
