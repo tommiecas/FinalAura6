8a31bed68a3ce74d71b6469b4282e4a46e86f86b
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-09-27 22:55:03 -0500

    FireBolt Projectile Spread

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -5,6 +5,7 @@
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AuraGameplayTags.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 FString UAuraFireBolt::GetDescription(int32 Level)
 {
@@ -104,4 +105,55 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 	Cooldown,
 	FMath::Min(Level, NumProjectiles),
 	ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag,
+	bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), SocketTag);
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+
+	FTransform SpawnTransform;
+	SpawnTransform.SetLocation(SocketLocation);
+	SpawnTransform.SetRotation(Rotation.Quaternion());
+	if (bOverridePitch) Rotation.Pitch = PitchOverride;
+
+	const FVector Forward = Rotation.Vector();
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectileSpread / 2.f, FVector::UpVector);
+	const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectileSpread / 2.f, FVector::UpVector);
+
+	//NumProjectiles = FMath::Min(MaxNumProjectiles, GetAbilityLevel());
+	
+	if (NumProjectiles > 1)
+	{
+		const float DeltaSpread = ProjectileSpread / (NumProjectiles - 1);
+		for (int32 i = 0; 1 < NumProjectiles; i++)
+		{
+			const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+			const FVector Start = SocketLocation + FVector(0,0,10);
+			UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
+				Start,
+				Start + Direction * 75.f,
+				1,
+				FLinearColor::Red,
+				120,
+				1);
+	
+		}
+	}
+	else
+	{
+		const FVector Start = SocketLocation + FVector(0,0,10);
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
+				Start,
+				Start + Forward * 75.f,
+				1,
+				FLinearColor::Red,
+				120,
+				1);
+	}
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + LeftOfSpread * 100.f, 1, FLinearColor::Blue, 120, 1);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), SocketLocation, SocketLocation + RightOfSpread * 100.f, 1, FLinearColor::Yellow, 120, 1);
+
+
 }
