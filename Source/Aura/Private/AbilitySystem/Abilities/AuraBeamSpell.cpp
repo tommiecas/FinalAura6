e9f4dfe8b8197ad5c2ee7d66a3d5eb88e2747bc9
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-09-28 22:52:28 -0500

    Aura Beam Spell: Part 1

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,26 @@
+// Copyright Gravity Well Games
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.ImpactPoint;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true);
+	}
+}
+
+void UAuraBeamSpell::StoreOwnerPlayerController()
+{
+	if (CurrentActorInfo)
+	{
+		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+	}
+	CurrentActorInfo->PlayerController;
+}
