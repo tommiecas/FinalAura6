7e6bfe6e3a35ada0ba01a67aa7eaeab1bf9e2eff
Tom Casiello <tomcasiello@gmail.com>
2024-10-14 23:52:40 -0700

    Data Structures for Saving Data

--- Source/Aura/Private/Game/LoadScreenSaveGame.cpp
+++ Source/Aura/Private/Game/LoadScreenSaveGame.cpp
@@ -3,3 +3,26 @@
 
 #include "Game/LoadScreenSaveGame.h"
 
+FSavedMap ULoadScreenSaveGame::GetSavedMapWithMapName(const FString& LevelName)
+{
+	for (const FSavedMap Map : SavedMaps)
+	{
+		if (Map.MapAssetName == LevelName)
+		{
+			return Map;
+		}
+	}
+	return FSavedMap();
+}
+
+bool ULoadScreenSaveGame::HasMap(const FString& LevelName)
+{
+	for (const FSavedMap Map : SavedMaps)
+	{
+		if (Map.MapAssetName == LevelName)
+		{
+			return true;
+		}
+	}
+	return false;
+}
