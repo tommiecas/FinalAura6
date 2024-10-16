5935c417615c6a3798985cd208d58cb50f276bee
Tom Casiello <tomcasiello@gmail.com>
2024-10-16 00:28:00 -0700

    Different Highlight Colors

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_CYAN 251
+#define CUSTOM_DEPTH_PURPLE 252
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 #define ECC_Enemy ECollisionChannel::ECC_GameTraceChannel2
 #define ECC_Target ECollisionChannel::ECC_GameTraceChannel3
